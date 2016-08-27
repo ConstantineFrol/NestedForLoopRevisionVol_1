ead926974c46254e44ccf9c8b442162801efe0a8
Cons†antine <simplemail84@gmail.com>
2016-08-27 02:20:28 +0100

    Add files via upload

---
+++ Week3_Lab1_forLoopRevision1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Practicing2ndYear;
+
+/**
+ * @author 		Konstantin Frolov
+ * Student ID 	R00144177
+ * Email		konstantin.frolov@mycit.ie
+ * GitHub		https://github.com/ConstantineFrol/repositories
+ */
+public class Week3_Lab1_forLoopRevision1 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 6; i++)
+		{
+			System.out.print(2 + i * 18 - 24 + "\t");
+		}
+		System.out.println();
+		for (int i = -4; i <= 86; i += 18)
+		{
+			System.out.print(i + "\t");
+		}
+	}
+}
